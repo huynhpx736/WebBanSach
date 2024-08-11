36db69b6685c2e9f59a03b42a9f9f83f462e1665
huynhpx736 <92499389+huynhpx736@users.noreply.github.com>
2024-08-11 22:15:14 +0700

    fix pages

--- bookshop-fe/src/index.js
+++ bookshop-fe/src/index.js
@@ -22,32 +22,3 @@ root.render(
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import store from './store';
-// import App from './App';
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>,
-//   document.getElementById('root')
-// );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
